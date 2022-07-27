e8c6fcad7437ae92260c284ff28e405978e4388e
Kayden Lee <kayden@programmer.net>
2022-07-27 07:33:36 +0000

    update

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
-const port = 3000;
+app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
+
+app.listen(process.env.PORT || 8080);
